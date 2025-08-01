35cad0cba96a05984eb9fc3affd0a6a902722899
Vaneesa Joju <vaneesajojuann@gmail.com>
2025-08-01 19:42:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Signup from './components/Signup'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          
+        </Routes>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
